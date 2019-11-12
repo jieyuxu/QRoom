7dd3de400296d4f82e7b5997692dd1ce5c19acae
jieyuxu <13708149+jieyuxu@users.noreply.github.com>
2019-11-12 12:00:02 -0500

    pushed sessions

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, session, url_for, redirect
 from flask_cas import CAS
 from flask_cas import login_required
 import os
@@ -8,18 +8,39 @@ app.secret_key = os.urandom(24)
 ########## CAS AUTHENTICATION ###########
 cas = CAS(app)
 app.config['CAS_SERVER'] = "https://fed.princeton.edu/cas/login"
-app.config['CAS_AFTER_LOGIN'] = 'profile'
+app.config['CAS_AFTER_LOGIN'] = 'reroute'
 app.config['CAS_AFTER_LOGOUT'] = 'http://localhost:5000'
 app.config['CAS_LOGIN_ROUTE'] = '/cas'
 #########################################
 
 @app.route('/')
 def index():
-   return render_template("index.html")
+   loggedin = False 
+   
+   if 'username' in session: 
+      loggedin = True 
+
+   return render_template("index.html" ,loggedin = loggedin)
+
+@app.route('/reroute')
+def reroute():
+   print(app.config['CAS_USERNAME_SESSION_KEY'])
+   if cas.username is not None:
+      print("user: ", cas.username) 
+      session['username'] = cas.username
+   
+   return redirect(url_for('profile'))
 
 @app.route('/profile')
 def profile():
-   return render_template("index.html")
+   loggedin = False 
+   if 'username' in session:
+      loggedin = True 
+      return render_template("profile.html", loggedin = loggedin, username = cas.username)
+
+   else:
+      return redirect(url_for("index"))
+   
 
 if __name__ == '__main__':
    app.run(debug=True)
