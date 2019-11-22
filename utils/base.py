4f116d946a470e3c64b951b4881029abb139a1f8
jieyuxu <13708149+jieyuxu@users.noreply.github.com>
2019-11-22 16:28:46 -0500

    alpha ver

--- utils/base.py
+++ utils/base.py
@@ -29,7 +29,7 @@ Base = declarative_base()
 # url = 'postgresql+psycopg2://user:password@hostname/mydb'
 
 # THIS URL IS FOR HEROKU POSTGRES. PLEASE COMMENT ONLY, DON'T DELETE
-url = os.environ('DATABASE_URL')
+url = os.environ['DATABASE_URL']
 
 # url = 'postgresql+psycopg2://bob:hi@localhost/mydb'
 engine = create_engine(url)
