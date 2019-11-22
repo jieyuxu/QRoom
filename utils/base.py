978d813453150b385e4c92ecc6e2404d81847895
jieyuxu <13708149+jieyuxu@users.noreply.github.com>
2019-11-21 21:15:51 -0500

    changed database url

--- utils/base.py
+++ utils/base.py
@@ -5,12 +5,12 @@ from sqlalchemy.ext.declarative import declarative_base
 
 Base = declarative_base()
 
-password = pgpasslib.getpass('localhost', 5555, 'qroom', 'postgres')
-if not password:
-    raise ValueError('Did not find a password in the .pgpass file')
+# password = pgpasslib.getpass('localhost', 5555, 'qroom', 'postgres')
+# if not password:
+#     raise ValueError('Did not find a password in the .pgpass file')
 
-url = 'postgresql://postgres:{}@localhost:5555/qroom'.format(password)
+# url = 'postgresql://postgres:{}@localhost:5555/qroom'.format(password)
 
-# url = 'postgresql://postgresql-rectangular-86196'
+url = 'postgresql://postgresql-rectangular-86196'
 engine = create_engine(url)
 session_factory = sessionmaker(bind=engine)
