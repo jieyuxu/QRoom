3b6e96c239140173de8e0f12d8bcd4dd6c64ee7e
jieyuxu <13708149+jieyuxu@users.noreply.github.com>
2019-11-22 02:58:15 -0500

    more admin fxnality

--- utils/base.py
+++ utils/base.py
@@ -1,18 +1,18 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
-# import pgpasslib
+import pgpasslib
 from sqlalchemy.ext.declarative import declarative_base
 import os
 
 Base = declarative_base()
 
-# password = pgpasslib.getpass('localhost', 5555, 'qroom', 'postgres')
-# if not password:
-#     raise ValueError('Did not find a password in the .pgpass file')
+password = pgpasslib.getpass('localhost', 5555, 'qroom', 'postgres')
+if not password:
+    raise ValueError('Did not find a password in the .pgpass file')
 
-# url = 'postgresql://postgres:{}@localhost:5555/qroom'.format(password)
+url = 'postgresql://postgres:{}@localhost:5555/qroom'.format(password)
 
 # url = 'postgresql://postgresql-rectangular-86196'
-url = os.environ['DATABASE_URL']
+# url = os.environ['DATABASE_URL']
 engine = create_engine(url)
 session_factory = sessionmaker(bind=engine)
