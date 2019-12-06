2d6786f1cdc2a4642d48c2c7e52031ec9d62a98b
Suki Yip <suki.yip.271@gmail.com>
2019-12-06 00:23:46 -0500

    date time fixed

--- inserts.py
+++ inserts.py
@@ -20,7 +20,7 @@ firestone = Buildings(building_name = "Firestone", latitude=40.3496, longitude=7
 frist = Buildings(building_name = "Frist", latitude=40.3468, longitude=74.6552)
 lewis = Buildings(building_name = "Lewis", latitude=40.3461, longitude=74.6526)
 
-group1 = Groups(open_time = time(8,00,00), close_time = time(00,00,00))
+group1 = Groups(open_time = time(8,00,00), close_time = time(5,00,00))
 group2 = Groups(open_time = time(8,00,00), close_time = time(20,00,00))
 
 
