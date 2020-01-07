bbb892ac978930e0eb66f053ab9cee384eb7cbb7
jieyuxu <13708149+jieyuxu@users.noreply.github.com>
2020-01-07 17:52:25 -0500

    location fix

--- static/js/location.js
+++ static/js/location.js
@@ -17,6 +17,21 @@ function error() {
   console.log("there is an error");
   // console.warn(`ERROR(${err.code}): ${err.message}`);
   alert("The application needs your current location to book a room.")
+  navigator.permissions.query({name:'geolocation'}).then(function(result) {
+    if (result.state == 'prompt') {
+      report(result.state);
+      geoBtn.style.display = 'none';
+      navigator.geolocation.getCurrentPosition(revealPosition,positionDenied,geoSettings);
+    } else if (result.state == 'denied') {
+      report(result.state);
+      geoBtn.style.display = 'inline';
+    }
+  });
+}
+
+function report(state) {
+  console.log('Permission ' + state);
+}
   window.location = '/booking';
 }
 
