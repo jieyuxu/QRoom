5c68d7ba44b73a14dc8145ef195020e5ea0e6854
jieyuxu <13708149+jieyuxu@users.noreply.github.com>
2020-01-11 22:05:01 -0500

    hs

--- static/js/location.js
+++ static/js/location.js
@@ -1,6 +1,6 @@
 window.onload = function () {
   navigator.permissions.query({name:'geolocation'}).then(function(result) {
-    while (result.state == 'prompt') {
+    if (result.state == 'prompt') {
       alert('Please allow this application to use your location. If you are on a mobile device, please turn on your GPS.');
       location.reload();
     } 
