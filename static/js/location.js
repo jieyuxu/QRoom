12ce9cd462f798f91ff7322921ffae2c33859fac
jieyuxu <13708149+jieyuxu@users.noreply.github.com>
2020-01-11 19:29:45 -0500

    location fix

--- static/js/location.js
+++ static/js/location.js
@@ -35,13 +35,15 @@ function checkPermission() {
 
 function error(state) {
   // checkPermission();
-  var r = confirm("The application needs your current location to book a room. Please turn on location services on your phone. Click 'OK' once you've done so. Clicking 'CANCEL' will take you back to your bookings page.");
+  if (state.code == 1) {
+    var r = confirm("The application needs your current location to book a room. Please turn on location services on your phone. Click 'OK' once you've done so. Clicking 'CANCEL' will take you back to your bookings page.");
     if (r == true) {
       location.reload();
     }
     else {
       window.location = '/booking'
     }
+  }
 }
 
 function distance(lat1, lon1, lat2, lon2, unit) {
